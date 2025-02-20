8a9b7a7a04baee248020a9a860b7a2334913c660
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:16:39 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -104,7 +104,7 @@ def lambda_handler(event, context):
     if str(path) != '' and str(http_method) != '':
         try:
             # Ejecutamos la función deseada
-            resultado = orquestador_lambda.ejecutar_funcion(path, http_method, body)
+            #resultado = orquestador_lambda.ejecutar_funcion(path, http_method, body)
             return json_response(httpStatusCode=200, body={'OK'})
         except Exception as e:
             return json_response(httpStatusCode=500, body={'error':f'Error {str(event)} ( path: {str(path)}. args: {str(body)}. {e} ).'})
