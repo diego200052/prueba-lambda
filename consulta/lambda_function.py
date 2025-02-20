1569b1867b4b2c0025b2640103cf6b3825db1e04
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:37:01 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -61,8 +61,8 @@ class OrquestadorLambda:
             if funcion.path == path and funcion.http_method == http_method:
                 # Ejecuta la función
                 args = self._convertir_body_a_dict(http_method, body)
-                return funcion.ejecutar(args)
-        return {}
+                #return funcion.ejecutar()
+        return {args}
         #return {path}
 
         #raise Exception('El path de la función no existe.')
@@ -117,5 +117,5 @@ def lambda_handler(event, context):
     #return json_response(httpStatusCode=500, body={'error':f'Path de la función vacío: ({path} {http_method}) {str(event)}'})
     return {
         'statusCode' : 200,
-        'body': str(path) + " --13- " + str(http_method) + " --- " + str(body) + str(resultado)
+        'body': str(path) + " --14- " + str(http_method) + " --- " + str(body) + str(resultado)
     }
