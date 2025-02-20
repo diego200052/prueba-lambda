2d79c2b05222822f809e86b1a55b30cf42d9162b
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:56:08 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -106,16 +106,16 @@ def lambda_handler(event, context):
         body = {}
 
     if str(path) != '' and str(http_method) != '':
-        #try:
+        try:
             # Ejecutamos la función deseada
-            #resultado = orquestador_lambda.ejecutar_funcion(path, http_method, body)
-        return json_response(httpStatusCode=200, body={'OK'})
-        #except Exception as e:
-        #    resultado = "excepcion"
-            #return json_response(httpStatusCode=500, body={'error':f'Error {str(event)} ( path: {str(path)}. args: {str(body)}. {e} ).'})
+            resultado = orquestador_lambda.ejecutar_funcion(path, http_method, body)
+            return json_response(httpStatusCode=200, body=resultado)
+        except Exception as e:
+            resultado = "excepcion"
+            return json_response(httpStatusCode=500, body={'error':f'Error {str(event)} ( path: {str(path)}. args: {str(body)}. {e} ).'})
     # path == ''
-    #return json_response(httpStatusCode=500, body={'error':f'Path de la función vacío: ({path} {http_method}) {str(event)}'})
-    return {
-        'statusCode' : 200,
-        'body': str(path) + " --25- " + str(http_method) + " --- " + str(body) + str(resultado)
-    }
+    return json_response(httpStatusCode=500, body={'error':f'Path de la función vacío: ({path} {http_method}) {str(event)}'})
+    # return {
+    #     'statusCode' : 200,
+    #     'body': str(path) + " --25- " + str(http_method) + " --- " + str(body) + str(resultado)
+    # }
