6ac17046226184453b4e6b4f0cf895147871686d
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:34:34 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -117,5 +117,5 @@ def lambda_handler(event, context):
     #return json_response(httpStatusCode=500, body={'error':f'Path de la función vacío: ({path} {http_method}) {str(event)}'})
     return {
         'statusCode' : 200,
-        'body': str(path) + " --11- " + str(http_method) + " --- " + str(body) + str(resultado)
+        'body': str(path) + " --12- " + str(http_method) + " --- " + str(body) + str(resultado)
     }
