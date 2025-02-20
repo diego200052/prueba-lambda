1e6630c87f4d5d69b7bb585712cd2db8ee3581ed
Diego M. <com1pac56@hotmail.com>
2025-02-20 19:49:53 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -2,17 +2,16 @@ import json
 from vehiculos import obtener_vehiculos
 
 # Aquí se deben agregar las funciones deseadas como una tripleta.
-# (Función, Path, Método HTTP)
+# (Función, Path)
 FUNCIONES = [
-    (obtener_vehiculos, 'vehiculos/obtener_vehiculos', 'GET')
+    (obtener_vehiculos, 'vehiculos/obtener_vehiculos')
 ]
 
 class Funcion:
     """
         Función a ejecutar en determinada path y con determinado método
     """
-    def __init__(self, func, path: str, httpMethod : str):
-        self.httpMethod = httpMethod
+    def __init__(self, func, path: str):
         self.path = path
         self.func = func
 
@@ -34,26 +33,26 @@ class OrquestadorLambda:
                                 tupla_funcion[1],
                                 tupla_funcion[2])
 
-    def agregar_funcion(self, func, path : str, httpMethod: str = 'GET'):
-        if (self._verificar_path(path, httpMethod)):
-            self.funciones.append(Funcion(func, path, httpMethod))
+    def agregar_funcion(self, func, path : str):
+        if (self._verificar_path(path)):
+            self.funciones.append(Funcion(func, path))
         else:
-            raise Exception(f'Hay una función con un path y método repetidos: ({path} {httpMethod})')
+            raise Exception(f'Hay una función con un path y método repetidos: ({path})')
 
-    def _verificar_path(self, path, httpMethod):
+    def _verificar_path(self, path):
         """
         Verifica si el path y el método ya se habían usado
         """
         for funcion in self.funciones:
-            if funcion.path == path and funcion.httpMethod == httpMethod:
+            if funcion.path == path:
                 return False
         return True
 
     
-    def ejecutar_funcion(self, httpMethod : str, path : str, args : dict):
+    def ejecutar_funcion(self, path : str, args : dict):
         # Busca la función dado el path y el método
         for funcion in self.funciones:
-            if funcion.httpMethod == httpMethod and funcion.path == path:
+            if funcion.path == path:
                 # Ejecuta la función
                 funcion.ejecutar(args)
 
@@ -72,24 +71,19 @@ def lambda_handler(event, context):
             'body': json.dumps({'error':f'Error al inicializar el orquestador lambda: {e}'})
         }
 
-    # Obtenemos el cuerpo de la petición
-    body = None
-    if 'body' in event:
-        body = event['body']
-
     #! Prueba, borrar después
-    return {
-        'statusCode' : 200,
-        'body': json.dumps(str(event) + "-----" + str(context))
-    }
+    # return {
+    #     'statusCode' : 200,
+    #     'body': json.dumps(str(event) + "-----" + str(context))
+    # }
 
     try:
         # Ejecutamos la función deseada
-        resultado = orquestador_lambda.ejecutar_funcion(event.httpMethod)
+        resultado = orquestador_lambda.ejecutar_funcion(event['path'], event['args'])
     except:
         return {
             'statusCode': 200,
-            'body': json.dumps({'error':f'Error al ejecutar la función. ( {event.httpMethod}).'})
+            'body': json.dumps({'error':f'Error al ejecutar la función. ( {str(event['path'])} con args: {str(event['args'])}).'})
         }
 
     return {
