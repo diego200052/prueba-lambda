7bf6193b4432a37c8c8b5df385d3ea3897b8a4cc
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:21:20 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -56,7 +56,15 @@ class OrquestadorLambda:
         return {}
     
     def ejecutar_funcion(self, path : str, http_method : str, body):
+        # Busca la función dado el path y el método
+        for funcion in self.funciones:
+            if funcion.path == path and funcion.http_method == http_method:
+                # Ejecuta la función
+                #return funcion.ejecutar(self._convertir_body_a_dict(body))
+                pass
         return {}
+
+        #raise Exception('El path de la función no existe.')
 
 
 def json_response(httpStatusCode : int = 500, body : dict = {}):
