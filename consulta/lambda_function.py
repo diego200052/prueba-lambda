411af8a435e5c8e6b255c2dfa604b4711e5dad22
Diego M. <com1pac56@hotmail.com>
2025-02-20 23:30:05 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -110,7 +110,7 @@ def lambda_handler(event, context):
             #return json_response(httpStatusCode=200, body={'OK'})
         except Exception as e:
             resultado = "excepcion"
-            return json_response(httpStatusCode=500, body={})
+            return json_response(httpStatusCode=200, body={})
     # path == ''
     #return json_response(httpStatusCode=500, body={'error':f'Path de la función vacío: ({path} {http_method}) {str(event)}'})
     return {
