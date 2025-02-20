b8df5538af528c749d55f4e9e9a1b6466632b6b7
Diego M. <com1pac56@hotmail.com>
2025-02-20 17:34:25 +0000

    .

--- lambda_function.py
+++ lambda_function.py
@@ -11,20 +11,5 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hola mundo')
-    }
-# import boto3
-import json
-
-def lambda_handler(event, context):
-    # s3 = boto3.client('s3')
-    # listObjSummary = s3.list_buckets()
-    # lista=[]
-    # for objSum in listObjSummary['Buckets']:
-    #     lista.append(objSum['Name'])
-    #     print(objSum)
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hola mundo 6')
+        'body': json.dumps('Hola mundo 7')
     }
