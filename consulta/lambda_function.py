01f55b4f3a3a3aeb24574becc175b2efe53175e1
Diego M. <com1pac56@hotmail.com>
2025-02-24 20:14:33 +0000

    .

--- consulta/lambda_function.py
+++ consulta/lambda_function.py
@@ -79,10 +79,9 @@ class OrquestadorLambda:
         if (type(body) == dict):
             return body
         if (type(body) == str and http_method == 'GET'):
-            newbody = str({key: value[0] for key, value in urllib.parse.parse_qs(body).items()})
-            newbody = newbody.replace("'", '"')
-            # if len(newbody) == 0:
-            newbody = json.loads(newbody)
+            newbody = {key: value[0] for key, value in urllib.parse.parse_qs(body).items()}
+            if len(newbody) == 0:
+                newbody = json.loads(body)
             return newbody
         return {}
     
